9144a0cbd246e8b50237cba9d0ebf3837699bdc2
trungit152 <ductrungg28ynpl@gmail.com>
2024-09-30 20:51:32 +0700

    lab 4.1

---
+++ 20215491_Lab4_1/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "20215491_Lab4_1"
+include(":app")
+ 
